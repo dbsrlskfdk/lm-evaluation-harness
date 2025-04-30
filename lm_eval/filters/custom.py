b35e4d07830d45db9130efa56f708f1af35fad8c
Yungi <dbsrlskfdk@naver.com>
2025-05-01 00:49:51 +0900

    Add Remove WhiteSpace Filters
    - Add Remove WhiteSpace Filter in Custom Filters
    - Add Remove WhiteSpace Filter in FilterList

--- lm_eval/filters/custom.py
+++ lm_eval/filters/custom.py
@@ -15,3 +15,19 @@ class CustomFilter(Filter):
 
     def apply(self, resps, docs):
         return self.filter_fn(resps, docs)
+
+
+@register_filter("remove_whitespace")
+class RemoveWhitespaceFilter(Filter):
+    """
+    Filter that removes leading and trailing whitespace from the model responses.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    def apply(self, resps, docs):
+        def remove_whitespace(resp):
+            return "".join(resp.split())
+
+        return map(lambda r: [remove_whitespace(r)], resps)
